d394a40c07e503ce31e2dc66b9d6b86267870b76
Joe Butler <j.j.butler@lancaster.ac.uk>
2022-03-15 18:15:04 +0000

    Added kinetic and potential energy functions and corresponding unit tests

--- single_pendulum.py
+++ single_pendulum.py
@@ -4,6 +4,15 @@ import numpy as np
 #Define the equation of state for a single pendulum
 def EoM(theta, l=1, g=9.81):
     #The parameters are the angle theta, the length l (default 1m) and the gravitational acceleration g (default 9.81ms^-2)
-    #The second derivative of theta (i.e. the angular acceleration) is given by the following equation
-    secondDeriv = -g*np.sin(theta)/l
-    return secondDeriv
+    #The second derivative of theta (i.e. the angular acceleration) is returned
+    return -g*np.sin(theta)/l
+
+def kineticEnergy(z, m=1, l=1):
+    #The parameters are z (defined as the first derivative of theta), bob mass m (default 1kg) and length l (default 1m)
+    #The kinetic energy of the pendulum is returned
+    return 0.5*m*(l**2)*(z**2)
+
+def potentialEnergy(theta, m=1, l=1, g=9.81):
+    #The parameters are the angle theta, mass m (default 1kg), length l (default 1m) and gravitational acceleration g (default 9.81ms^-2)
+    #The potential energy of the pendulum is returned
+    return -m*g*l*np.cos(theta)
--- test_single_pendulum.py
+++ test_single_pendulum.py
@@ -4,9 +4,29 @@ import numpy as np
 
 #Test the EoM function
 def test_EoM():
-    #Check that when the pendulum is horizontal, sin(theta)=1 and so the angular acceleration = -g/l
+    #Check that when the pendulum is horizontal, sin(theta)=1 and so the angular acceleration = -g/l:
     assert sp.EoM(np.pi/2, 1, 9.81) == -9.81
-    #Check that when the pendulum is vertical, sin(theta)=0 and so the angular acceleration = 0
+    #Check that when the pendulum is vertical, sin(theta)=0 and so the angular acceleration = 0:
     assert sp.EoM(0, 1, 9.81) == 0
 
-test_EoM()
+#test_EoM()
+
+#Test the kineticEnergy function
+def test_kineticEnergy():
+    #Check that when the pendulum is horizontal (i.e. z=0 since the pendulum velocity will be 0), the kinetic energy is 0:
+    assert sp.kineticEnergy(0, 1, 1) == 0
+    #An equation for the first derivative of theta is z=sqrt(-2*g*y)/l. When l=1 and the pendulum is vertical (so y=-l=-1):
+    z=np.sqrt(-2*9.81*-1)/1
+    #Check that when the pendulum is vertical and m=l=1, the kinetic energy is g=9.81
+    assert sp.kineticEnergy(z, 1, 1) == 9.81
+
+#test_kineticEnergy()
+
+#Test the potentialEnergy function
+def test_potentialEnergy():
+    #Check that when the pendulum is horizontal (i.e. theta=pi/2), the potential energy is approximately 0:
+    assert np.abs(sp.potentialEnergy(np.pi/2, 1, 1, 9.81)) <= 1e-10
+    #Check that when the pendulum is vertical (i.e. theta=0) and l=1 and m=1, the potential energy is -g=-9.81:
+    assert sp.potentialEnergy(0, 1, 1, 9.81) == -9.81
+
+test_potentialEnergy()
